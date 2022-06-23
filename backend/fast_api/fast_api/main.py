8e7b572d93a89d389c779d3b9bc6c1fdc524a994
Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>
2022-06-23 21:43:15 +0200

    [BACKEND]: adding middlewares + test API

---
+++ backend/fast_api/fast_api/main.py
@@ -0,0 +1,68 @@
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from requests import HTTPError
+from fast_api import settings
+from fast_api.routers.hello import router as test_router
+import secrets
+from fastapi import status, HTTPException, Depends
+from fastapi.security import HTTPBasicCredentials, HTTPBasic
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
+
+http_basic = HTTPBasic()
+
+
+def authorize(credentials: HTTPBasicCredentials = Depends(http_basic)) -> str:
+    if settings.HTTP_BASIC_USERNAME and settings.HTTP_BASIC_PASSWORD:
+        correct_username = secrets.compare_digest(credentials.username, settings.HTTP_BASIC_USERNAME)
+        correct_password = secrets.compare_digest(credentials.password, settings.HTTP_BASIC_PASSWORD)
+        if not (correct_username and correct_password):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Incorrect username or password",
+                headers={"WWW-Authenticate": "Basic"},
+            )
+        return credentials.username
+
+
+app = FastAPI()
+
+
+# custom error handling
+@app.exception_handler(HTTPError)
+async def requests_exception_handler(request: Request, exc: HTTPError):
+    return JSONResponse(
+        status_code=exc.response.status_code,
+        content=exc.response.json(),
+    )
+
+
+@app.get("/", tags=["Index"], dependencies=[Depends(authorize)])
+async def index():
+    return {"message": "Hello World"}
+
+
+@app.get("/me")
+def get_current_user(username: str = Depends(authorize)):
+    return {"username": username}
+
+
+# Add routers
+if settings.DEBUG:  # only in debug mode
+    app.include_router(test_router, prefix="/api", dependencies=[Depends(authorize)])
+
+# Add middlewares
+if settings.ALLOWED_HOSTS:
+    app.add_middleware(
+        TrustedHostMiddleware,
+        allowed_hosts=settings.ALLOWED_HOSTS,
+    )
+
+
+def start():
+    """Launched with `poetry run start` at root level"""
+    uvicorn.run("fast_api.main:app", host="0.0.0.0", port=8000, reload=True)
+
+
+if __name__ == "__main__":
+    start()
