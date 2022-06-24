2e332ac39b0f27a63fc8d2428df13d8d70c48062
Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>
2022-06-24 11:31:43 +0200

    [BACKEND]: fix Models

--- backend/fast_api/fast_api/models/__init__.py
+++ backend/fast_api/fast_api/models/__init__.py
@@ -7,14 +7,14 @@ class FormatDTO(pd.BaseModel):
 
 
 class RunDTO(pd.BaseModel):
-    code: str
+    code: str = pd.Field(description='Code to run, example: printf "Hello, World!"')
     stdin: t.Optional[str]
     problem_id: t.Optional[str]
 
 
 class RunRO(pd.BaseModel):
-    cpu_time: t.Optional[float]
-    memory: t.Optional[float]  # in KB
+    cpu_time: t.Optional[float] = pd.Field(description='CPU time in seconds')
+    memory: t.Optional[int] = pd.Field(description='Memory usage in kilobytes (KB)')
     is_correct: t.Optional[bool]
     stdout: t.Optional[str]
-    stderr: t.Optional[str]
+    # stderr: t.Optional[str]
